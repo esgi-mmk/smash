800328a6300c5e5001377fdd56fea8f058f410bd
Weji <1william.morgado@gmail.com>
2019-05-03 10:56:17 +0200

    fix hit detection

--- arduino/smash_sensor/smash_sensor.ino
+++ arduino/smash_sensor/smash_sensor.ino
@@ -11,16 +11,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 char ssid[] = "William";                // TODO: your network SSID (name)
 char pass[] = "toto1shsh";             // TODO: your network password
-const IPAddress outIp(172,20,10,2); // TODO: remote IP of your computer
+const IPAddress outIp(172, 20, 10, 2); // TODO: remote IP of your computer
 ////////////////////////////////////////////////////////////////////////////////
 
 const float MIN_VALUE_FOR_2G = 8.00;
 const float MIN_VALUE_FOR_4G = 4.50;
 const float MIN_VALUE_FOR_8G = 3.50;
 
-volatile byte interruptCounter = 0;
+volatile bool d5_touched, d6_touched, d7_touched = false;
 int numberOfInterrupts = 0;
-
 
 const int VALUE_RESCALE_RATE = 4;
 const unsigned int localPort = 8888;  // local port to listen for OSC packets (not used for sending)
@@ -29,7 +28,6 @@ TwoWire i2c;
 
 WiFiUDP Udp;                          // A UDP instance to let us send and receive packets over UDP
 
-
 MPU6050 mpu6050(i2c);
 
 long timeStamp = 0;
@@ -37,117 +35,166 @@ bool flag = false;
 
 
 void setup() {
-    Serial.begin(115200);
-    i2c.begin(D3, D4);
-    
-    mpu6050.begin();
-    //mpu6050.writeMPU6050(MPU6050_ACCEL_CONFIG, 0x18); // 16g
-    mpu6050.writeMPU6050(MPU6050_ACCEL_CONFIG, 0x10); // 8g
-    //mpu6050.writeMPU6050(MPU6050_ACCEL_CONFIG, 0x08); // 4g
+  Serial.begin(115200);
+  i2c.begin(D3, D4);
 
-    /*
-    // Connect to WiFi network
-    Serial.print("\nConnecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, pass);
+  mpu6050.begin();
+  //mpu6050.writeMPU6050(MPU6050_ACCEL_CONFIG, 0x18); // 16g
+  mpu6050.writeMPU6050(MPU6050_ACCEL_CONFIG, 0x10); // 8g
+  //mpu6050.writeMPU6050(MPU6050_ACCEL_CONFIG, 0x08); // 4g
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("\nWiFi connected - IP address: ");
-    Serial.println(WiFi.localIP());
 
-    Serial.println("Starting UDP");
-    Udp.begin(localPort);
+  
+  // Connect to WiFi network
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
 
-    Serial.print("Local port: ");
-    Serial.println(Udp.localPort());
-    */
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected - IP address: ");
+  Serial.println(WiFi.localIP());
 
-    pinMode(D5, INPUT_PULLUP);
-    pinMode(D6, INPUT_PULLUP);
-    pinMode(D7, INPUT_PULLUP);
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
 
-    attachInterrupt(digitalPinToInterrupt(D5), onTouchD5, FALLING);
-    attachInterrupt(digitalPinToInterrupt(D6), onTouchD6, FALLING);
-    attachInterrupt(digitalPinToInterrupt(D7), onTouchD7, FALLING);
-    
-    pinMode(LED_BUILTIN, OUTPUT);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
+ 
+
+  pinMode(D5, INPUT_PULLUP);
+  pinMode(D6, INPUT_PULLUP);
+  pinMode(D7, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(D5), onTouchD5, FALLING);
+  attachInterrupt(digitalPinToInterrupt(D6), onTouchD6, FALLING);
+  attachInterrupt(digitalPinToInterrupt(D7), onTouchD7, FALLING);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
 
 }
 
 void onTouchD5() {
-  float result = getPressure();
-  Serial.println(result);
-  
+  //float result = getPressure();
+  //Serial.println(result);
+  //delay(1000);
+  d5_touched = true;
 }
 
 void onTouchD6() {
-  float result = getPressure();
-  Serial.println(result);
+  //float result = getPressure();
+  //Serial.println(result);
+  //delay(1000);
+  d6_touched = true;
 }
 
 void onTouchD7() {
-  float result = getPressure();
-  Serial.println(result);
+  //float result = getPressure();
+  //Serial.println(result);
+  //delay(1000);
+  d7_touched = true;
 }
 
 
 void loop() {
-    // loop every 10 ms;
-    /*if(millis() - timeStamp > 10){
-      mpu6050.update();
-      timeStamp = millis();
+  // loop every 10 ms;
+  if (millis() - timeStamp > 10) {
+    mpu6050.update();
+    timeStamp = millis();
+
+    if (d5_touched == true || d6_touched == true || d7_touched == true) {
       
-      int areaTouched = getAreaTouched();
+      int area = -1;
+      if (d5_touched) {
+        area = 0;
+      }
       
-      if(areaTouched >= 0){
-        OSCMessage msg("/test");
-        float maxValue = 0.00;
-
-        //set a timer to count 1000 ms
-        long timer = millis();
-        while(!flag){
-          
-          float value = checkPressure();
-          if(maxValue <= value){
-            maxValue = value; 
-          }
-          
-          if(millis() - timer > 1000){
-            flag = !flag;
-          }
-        }
-        flag = !flag;
-        Udp.beginPacket(outIp, outPort);
-        msg.add(areaTouched);
-        msg.add(maxValue);
-        msg.send(Udp);
-
-        Serial.print("Message sended : (maxValue : ");
-        Serial.print(maxValue);
-        Serial.print(", areaTouched : ");
-        Serial.print(areaTouched);
-        Serial.println(")");
-        
-        Udp.endPacket(); 
-        msg.empty();
-
-      } else {
-        //Serial.println("Waiting for button press")
+      if (d6_touched) {
+        area = 1;
+      }
+      
+      if (d7_touched) {
+        area = 2;
       }
 
-    }*/
+      //float res = getPressure();
+      float maxValue = 0.00;
+
+      //set a timer to count 1000 ms
+      long timer = millis();
+      while(!flag){
+
+        float value = checkPressure();
+        if(maxValue <= value){
+          maxValue = value;
+        }
+
+        if(millis() - timer > 1000){
+          flag = !flag;
+        }
+      }
+      flag = !flag;
+
+      
+      Serial.println(maxValue);
+      d5_touched = false;
+      d6_touched = false;
+      d7_touched = false;
+
+      sendValues(maxValue, area);
+
+      //delay(2000);
+    }
+
+    /*
+      if(areaTouched >= 0){
+      //OSCMessage msg("/test");
+      float maxValue = 0.00;
+
+      //set a timer to count 1000 ms
+      long timer = millis();
+      while(!flag){
+
+        float value = checkPressure();
+        if(maxValue <= value){
+          maxValue = value;
+        }
+
+        if(millis() - timer > 1000){
+          flag = !flag;
+        }
+      }
+      flag = !flag;
+      Udp.beginPacket(outIp, outPort);
+      msg.add(areaTouched);
+      msg.add(maxValue);
+      msg.send(Udp);
+
+      Serial.print("Message sended : (maxValue : ");
+      Serial.print(maxValue);
+      Serial.print(", areaTouched : ");
+      Serial.print(areaTouched);
+      Serial.println(")");
+
+      Udp.endPacket();
+      msg.empty();
+
+      } else {
+      //Serial.println("Waiting for button press")
+      }*/
+
+  }
 }
 
-int getAreaTouched(){
-  
+int getAreaTouched() {
+
   boolean btnPressed1 = !digitalRead(D5);
   boolean btnPressed2 = !digitalRead(D6);
   boolean btnPressed3 = !digitalRead(D7);
-  
+
   if (btnPressed1 == true) {
     digitalWrite(LED_BUILTIN, LOW);
     return 0;
@@ -161,7 +208,7 @@ int getAreaTouched(){
   } else {
     digitalWrite(LED_BUILTIN, HIGH);
   }
-  
+
   if (btnPressed3 == true) {
     digitalWrite(LED_BUILTIN, LOW);
     return 2;
@@ -172,39 +219,54 @@ int getAreaTouched(){
   return -1;
 }
 
-float getPressure(){
+float getPressure() {
   float maxValue = 0.0;
   bool flag = false;
-  long timer = millis(); 
-  
-  while(!flag){
+  long timer = millis();
+
+  while (!flag) {
     float value = checkPressure();
     Serial.println(value);
-    if(maxValue <= value){
-      maxValue = value; 
+    if (maxValue <= value) {
+      maxValue = value;
     }
-    
-    if(millis() - timer > 1000){
+
+    if (millis() - timer > 1000) {
       flag = !flag;
     }
   }
 }
 
-float checkPressure(){
+float checkPressure() {
   float axes[3] = {fabs(mpu6050.getAccX()) * VALUE_RESCALE_RATE, fabs(mpu6050.getAccY()) * VALUE_RESCALE_RATE, fabs(mpu6050.getAccZ()) * VALUE_RESCALE_RATE};
-  
+
   int size_axes = sizeof(axes) / sizeof(axes[0]);
   int i = 0;
   float maxValue = 0.0;
-  
-  while (i < size_axes){
-    if(maxValue < axes[i]) {
+
+  while (i < size_axes) {
+    if (maxValue < axes[i]) {
       maxValue = axes[i];
     }
-    
     i++;
   }
-  
-  mpu6050.update();
+
   return maxValue;
+}
+
+void sendValues(float impact, int area) {
+  OSCMessage msg("/test");
+  Udp.beginPacket(outIp, outPort);
+  msg.add(area);
+  msg.add(impact);
+  msg.send(Udp);
+
+  Serial.print("Message sended (impact : ");
+  Serial.print(impact);
+  Serial.print(", area : ");
+  Serial.print(area);
+  Serial.println(")");
+
+  Udp.endPacket();
+  msg.empty();
 }
--- processing/App/App.pde
+++ processing/App/App.pde
@@ -32,7 +32,7 @@ void setup() {
   b1 = color(255); b2 = color(0); c1 = color(204, 102, 0); c2 = color(0, 102, 153);
   
   /* start oscP5, listening for incoming messages at port 4559 */
-  //oscP5 = new OscP5(this,4559);
+  oscP5 = new OscP5(this,4559);
   
   fightSystem = new FightSystem(2, 30.0);
 
@@ -47,11 +47,7 @@ void draw() {
   fightScene.setGradient(width/2, 0, width/2, height, b2, b1, X_AXIS);
   fightScene.setPlayersName("Player 1", "Player 2");
   
-  oscEventFake();
-  oscEventFake();
-  oscEventFake();
-  oscEventFake();
-  oscEventFake();
+  
   
 }
 
@@ -61,6 +57,9 @@ void oscEvent(OscMessage theOscMessage) {
 
   int zone = theOscMessage.get(0).intValue();
   float str = theOscMessage.get(1).floatValue();
+  
+  println(zone);
+  println(str);
   
   fightSystem.calculateDamage(playerTurn, zone, str);
   fightScene.printDamage(playerTurn, zone, str);
